616f4c864ebd1b7c5455a9f5f8df94a35659d413
HXY-0227 <1473706220@qq.com>
2019-01-05 11:18:59 +0800

    change dataSource

--- src/main/java/com/honeybee/controller/UserController.java
+++ src/main/java/com/honeybee/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.honeybee.controller;
 
 import com.honeybee.common.bean.UserBean;
-import com.honeybee.common.database.DynamicDataSourceContextHolder;
-import com.honeybee.common.database.TargetDataSource;
+import com.honeybee.dao.UserMapper;
 import com.honeybee.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,26 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
-    private UserService service;
+    private UserService userService;
 
     private Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    /*@PostMapping("/register")
-    public HoneyResult register(UserBean user){
-        try {
-            HoneyResult result =  service.register(user);
-            return result;
-        }catch (Exception e) {
-            return HoneyResult.build(400,ExceptionUtil.getStackTrace(e));
-        }
-    }*/
-
     @GetMapping("/select")
-    @TargetDataSource("master")
     public UserBean xxxx(){
         try {
-            UserBean result =  service.select();
-            logger.error(DynamicDataSourceContextHolder.getDataSourceKey()+"22");
+            UserBean result =  userService.select();
             return result;
         }catch (Exception e) {
             logger.error(e.getMessage());
--- src/main/java/com/honeybee/service/impl/UserServiceImpl.java
+++ src/main/java/com/honeybee/service/impl/UserServiceImpl.java
@@ -9,54 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional(rollbackFor = Exception.class)
+@Service(value = "userService")
 public class UserServiceImpl implements UserService {
 
     private Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
-    private UserMapper mapper;
-
-    /**
-     * 检查数据是否可用
-     * @return
-     */
-    /*@Override
-    public HoneyResult checkData() {
-        return null;
-    }*/
-
-    /**
-     * 用户注册
-     * @return
-     */
-    /*@Override
-    public HoneyResult register(UserBean user) {
-        try {
-            user.setCreateTime(new Date());
-            user.setUpdateTime(new Date());
-            //密码加密存储
-            user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes()));
-            mapper.insertUser(user);
-            return HoneyResult.ok();
-        } catch (Exception e) {
-            return HoneyResult.build(400, ExceptionUtil.getStackTrace(e));
-        }
-
-    }*/
-
-    /**
-     * 用户登录
-     * @return
-     */
-    /*@Override
-    public HoneyResult login(String phone, String password) {
-        return null;
-    }*/
+    private UserMapper userMapper;
 
     @Override
     public UserBean select() {
-        return mapper.selectByUserName("HXY-0227");
+        try {
+            return userMapper.selectByUserName("HXY");
+        }catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return null;
     }
 }
