ad559d5899791cc067f520afca14da656fd69520
HXY-0227 <1473706220@qq.com>
2019-03-24 10:23:51 +0800

    getIpAddress

--- src/main/java/com/honeybee/common/Filter/CsrfFilter.java
+++ src/main/java/com/honeybee/common/Filter/CsrfFilter.java
@@ -20,6 +20,7 @@ public class CsrfFilter extends OncePerRequestFilter {
 
     private final static String REQUEST_METHOD_POST = "POST";
     private final static String REQUEST_METHOD_GET = "GET";
+    private final static String UNKNOWN = "unknown";
     private final static Logger logger = LoggerFactory.getLogger(CsrfFilter.class);
 
     @Override
@@ -27,21 +28,50 @@ public class CsrfFilter extends OncePerRequestFilter {
 
         logger.info("Begin csrf...");
 
-        //服务器端存储的token
+        // 服务器端存储的token
         String token_server = (String) request.getSession().getAttribute("token");
-        //客户端请求的token
+        // 客户端请求的token
         String token_client = (String) request.getAttribute("token");
+
+        // 客户端ip
+        String ipAddress = getIpAddress(request);
 
         //如果是post请求，则做csrf校验
         if (REQUEST_METHOD_POST.equals(request.getMethod())) {
             if (null != token_client && null != token_server && token_client.equals(token_server)) {
                 filterChain.doFilter(request, response);
+                logger.info("Csrf parameter is valid...");
+                logger.info("User ip is " + ipAddress);
             }
             logger.info("Csrf parameter is invalid...");
+            logger.info("User ip is " + ipAddress);
         }else if (REQUEST_METHOD_GET.equals(request.getMethod())){
             filterChain.doFilter(request,response);
         }
 
         logger.info("End Csrf...");
+    }
+
+    /**
+     * 多级代理的情况下获取客户端真实ip
+     * @param request http请求
+     * @return 真实ip
+     */
+    public static String getIpAddress(HttpServletRequest request) {
+
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if (null == ipAddress || ipAddress.isEmpty() || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (null == ipAddress || ipAddress.isEmpty() || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if ((null != ipAddress) && (!ipAddress.isEmpty()) && (ipAddress.indexOf(",") > -1)) {
+            String[] ipArray = ipAddress.split(",");
+            ipAddress = ipArray[0];
+        }
+
+        return ipAddress;
     }
 }
