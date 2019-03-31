5dfca92da41f57fd51561ceafae410feeadc5c27
HXY-0227 <1473706220@qq.com>
2019-03-31 21:38:33 +0800

    springboot and redis

--- src/main/java/com/honeybee/common/configure/RedisConfigure.java
+++ src/main/java/com/honeybee/common/configure/RedisConfigure.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -19,7 +18,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * @author HXY
  */
 @Configuration
-@AutoConfigureAfter(RedisAutoConfiguration.class)
+@AutoConfigureAfter(RedisAutoConfiguration.class)  //redis自动配置
 public class RedisConfigure extends CachingConfigurerSupport {
 
     @Bean
@@ -29,11 +28,23 @@ public class RedisConfigure extends CachingConfigurerSupport {
         redisTemplate.setConnectionFactory(factory);
         // key的序列化类型
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper objectMapper = new ObjectMapper();
+        // PropertyAccessor.ALL指定要序列化的域，field,get和set
+        // JsonAutoDetect.Visibility.ANY表示修饰符范围，ANY是包括private和public
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-        // value的序列化类型
+
+        /**
+         * 序列化策略：
+         * 1.  默认使用JdkSerializationRedisSerializer序列化，存储的是字节数组，无法阅读，但空间小
+         * 2.  StringRedisSerializer：Key或者value为字符串的场景，对：new String(bytes, charset)和string.getBytes(charset)进行封装。轻量级和高效的策略。
+         * 3.  JacksonJsonRedisSerializer：将java对象序列化成json格式存储在redis中，也可以将json格式的数据转换成java对象。
+         * 4.  jackson2JsonRedisSerializer： 和JacksonJsonRedisSerializer差不多
+         * 5.  OxmSerializer：提供了将javabean与xml之间的转换能力
+         *
+         */
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
 
         return redisTemplate;
