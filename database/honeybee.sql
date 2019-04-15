70a812bde404a6ad51e274bf7052d3ba643e2664
HXY-0227 <1473706220@qq.com>
2019-04-15 23:17:24 +0800

    modify table

--- database/honeybee.sql
+++ database/honeybee.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `team_info` (
-  `team_id` int(10) NOT NULL AUTO_INCREMENT,
+  `team_id` int(10) NOT NULL,
   `team_name` varchar(15) NOT NULL COMMENT '团队名',
   `user_name` varchar(15) NOT NULL COMMENT '用户名',
   `captial` float DEFAULT NULL COMMENT '资金',
@@ -7,7 +7,7 @@ CREATE TABLE `team_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='团队信息表';
 
 CREATE TABLE `user_info` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `user_name` varchar(15) NOT NULL COMMENT '用户名',
   `user_password` varchar(30) NOT NULL COMMENT '用户密码',
   `user_phone` varchar(11) DEFAULT NULL COMMENT '用户电话',
@@ -17,7 +17,7 @@ CREATE TABLE `user_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
 
 CREATE TABLE `detail_info` (
-  `detail_id` int NOT NULL AUTO_INCREMENT,
+  `detail_id` int NOT NULL,
   `team_name` varchar(15) NOT NULL COMMENT '团队名',
   `user_name` varchar(15) NOT NULL COMMENT '用户名',
   `detail_type` char(1) NOT NULL COMMENT '明细类型',
