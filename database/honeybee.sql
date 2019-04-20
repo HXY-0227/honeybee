70cd97275f44b4e80567b8b4f81ba8133c9c9f65
yangjun1qaz <1094557609@qq.com>
2019-04-20 22:39:04 +0800

    表结构修改

--- database/honeybee.sql
+++ database/honeybee.sql
@@ -1,10 +1,3 @@
-CREATE TABLE `team_info` (
-  `team_id` int(19) NOT NULL,
-  `team_name` varchar(15) NOT NULL COMMENT '团队名',
-  `user_name` varchar(15) NOT NULL COMMENT '用户名',
-  `captial` float DEFAULT NULL COMMENT '资金',
-  PRIMARY KEY (`team_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='团队信息表';
 
 CREATE TABLE `user_info` (
   `user_id` varchar(20) NOT NULL,
@@ -16,12 +9,19 @@ CREATE TABLE `user_info` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
 
-CREATE TABLE `detail_info` (
-  `detail_id` int NOT NULL,
-  `team_name` varchar(15) NOT NULL COMMENT '团队名',
-  `user_name` varchar(15) NOT NULL COMMENT '用户名',
-  `detail_type` char(1) NOT NULL COMMENT '明细类型',
-  `captial` float DEFAULT NULL COMMENT '消费金额',
+
+CREATE TABLE `user_account` (
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `total_money` double(20,0) DEFAULT NULL COMMENT '用户总金额',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`detail_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='明细信息表';
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户账户表';
+
+
+CREATE TABLE `user_account_details` (
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',
+  `money` double(10,0) DEFAULT NULL COMMENT '金额',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户流水表';
+
