30cd875cafeed679bc5a5b47db4cc995cbb85d4d
HXY-0227 <1473706220@qq.com>
2019-04-23 23:20:30 +0800

    issue

--- src/main/java/com/honeybee/controller/CustomerInfoController.java
+++ src/main/java/com/honeybee/controller/CustomerInfoController.java
@@ -5,6 +5,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.honeybee.common.bean.CustomerInfoBean;
 import com.honeybee.common.bean.HoneyResult;
 import com.honeybee.service.CustomerInfoService;
+import com.honeybee.utils.RedisUtil;
+import com.honeybee.utils.SpringContextUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,9 +47,8 @@ public class CustomerInfoController {
         String token = request.getHeader("token");
         JSONObject requestData = JSON.parseObject(requestBody);
         String timeType = requestData.getString("timeType");
-        //RedisUtil redis = SpringContextUtil.getInstance().getBeanByClass(RedisUtil.class);
-        //String userId = String.valueOf(redis.get(baseKey + token));
-        String userId ="1";
+        RedisUtil redis = SpringContextUtil.getInstance().getBeanByClass(RedisUtil.class);
+        String userId = String.valueOf(redis.get(baseKey + token));
         CustomerInfoBean userInfoBean = customerInfoService.queryUserTotalMoney(userId,timeType);
         return HoneyResult.ok(userInfoBean);
     }
